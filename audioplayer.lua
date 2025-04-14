c8de7a87b63b3e6616c0d6c69db7b32be371bac4
Piotrekos69-Official <piotrekos69sgithub@gmail.com>
2025-04-14 20:16:00 +0200

    Update audioplayer.lua

--- audioplayer.lua
+++ audioplayer.lua
@@ -1,17 +1,25 @@
-
 local dfpwm = require("cc.audio.dfpwm")
 local decoder = dfpwm.make_decoder()
-local speakers = {peripheral.find("speaker")}
+local speaker = peripheral.find("speaker")
 
-for chunk in io.lines("sounds/1.dfpwm", 16 * 1024) do
-    local buffer = decoder(chunk)
-    local play_functions = {}
-    for _, speaker in pairs(speakers) do
-        table.insert(play_functions, function()
-            while not speaker.playAudio(buffer) do
-                os.pullEvent("speaker_audio_empty")
-            end
-        end)
-    end
-    parallel.waitForAll(table.unpack(play_functions))
+-- Tu podaj adres WAV z internetu
+local wavUrl = "https://raw.githubusercontent.com/Piotrekos69-Official/ComputerCraftMusic/main/Toby-Fox-Megalovania%20(1).wav"
+
+-- Pobierz dane strumieniowo z internetu
+local request = http.get(wavUrl, nil, true)
+if not request then
+    print("Błąd: Nie można połączyć się z URL")
+    return
 end
+
+while true do
+    local chunk = request.read(16 * 1024)
+    if not chunk then break end
+
+    local buffer = decoder(chunk)
+    while not speaker.playAudio(buffer) do
+        os.pullEvent("speaker_audio_empty")
+    end
+end
+
+request.close()
