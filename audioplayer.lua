a9cc103e6b66aae58a7ce9c98faf226631080884
Piotrekos69-Official <piotrekos69sgithub@gmail.com>
2025-04-14 20:51:32 +0200

    Update audioplayer.lua

--- audioplayer.lua
+++ audioplayer.lua
@@ -1,19 +1,30 @@
-local url = "ttps://remote.craftos-pc.cc/music/content/s88IU8_Y.wav"
-local request = http.get(url, nil, true)
+local dfpwm = require("cc.audio.dfpwm")
+local decoder = dfpwm.make_decoder()
+local speakers = {peripheral.find("speaker")}
 
-if not request then
-    print("Błąd połączenia!")
+local url = "https://remote.craftos-pc.cc/music/content/mJjKg4p0.wav"
+local handle = http.get(url, nil, true)
+if not handle then
+    print("Nie udało się połączyć z URL!")
     return
 end
 
-local tmpFile = "tmp_downloaded.wav"
-local f = fs.open(tmpFile, "wb")
-
 while true do
-    local chunk = request.read(4 * 1024)
+    local chunk = handle.read(4 * 1024) -- zmniejszony rozmiar
     if not chunk then break end
-    f.write(chunk)
+
+    local buffer = decoder(chunk)
+
+    local play_tasks = {}
+    for _, speaker in pairs(speakers) do
+        table.insert(play_tasks, function()
+            while not speaker.playAudio(buffer) do
+                os.pullEvent("speaker_audio_empty")
+            end
+        end)
+    end
+
+    parallel.waitForAll(table.unpack(play_tasks))
 end
 
-f.close()
-request.close()
+handle.close()
